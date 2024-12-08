1927d84883e06e253d5d78349aee8f18091c0efc
Karim Farahat <karimhosny99@gmail.com>
2024-12-08 21:57:31 +0200

    ignore dist

--- backend/dist/tasks/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tasks = void 0;
-var tasks_route_1 = require("./tasks.route");
-Object.defineProperty(exports, "tasks", { enumerable: true, get: function () { return tasks_route_1.router; } });
--- backend/dist/users/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-var users_route_1 = require("./users.route");
-Object.defineProperty(exports, "users", { enumerable: true, get: function () { return users_route_1.router; } });
